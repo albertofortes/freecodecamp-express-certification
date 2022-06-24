48387cbff5cf7e4c9ccd8ee697f6855bea48748e
Alberto Fortes Sanchez <albertofortes@MacBook-Pro-de-Alberto.local>
2022-06-24 13:35:44 +0200

    Serve an HTML File

--- myApp.js
+++ myApp.js
@@ -4,9 +4,10 @@ let app = express();
 console.log('Hello World');
 
 app.get("/", (req, res) => {
-  res.send("Hello Express");
+  //res.send("Hello Express");
+  let path = absolutePath = __dirname + '/views/index.html';
+  res.sendFile(path)
 });
-
 
 
 
